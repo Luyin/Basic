e208e496639ea0336dcbce788196db3caf55cc21
Luyin-PC\Luyin <myraous@gmail.com>
2017-06-20 08:06:44 +0900

    Merge AscendingSort and DescendingSort of BubbleSort

--- src/main/java/Sort/BubbleSort.java
+++ src/main/java/Sort/BubbleSort.java
@@ -4,38 +4,35 @@ package Sort;
  * Created by myrao_000 on 2017-02-05.
  */
 public class BubbleSort {
-    private static void Swap(int[] list, int i, int j)
+    private static void swap(int[] intArray, int i, int j)
     {
-        int temp = list[i];
-        list[i] = list[j];
-        list[j] = temp;
+        int temp = intArray[i];
+        intArray[i] = intArray[j];
+        intArray[j] = temp;
+
     }
 
-    //큰 값을 오른쪽으로 몰아낸다.
-    public static void AscendingSort(int[] list) {
-        for (int i=0; i<list.length; i++)
+    public static void sort(int[] intArray, boolean isAscending)
+    {
+        for(int i=0; i<intArray.length; i++)
         {
-            for(int j=0; j<list.length - (i + 1); j++)
+            for(int j=0; j<intArray.length - (i+1); j++)
             {
-                if (list[j] > list[j + 1]) {
-                    Swap(list, j, j+1);
-                }
-            }
-        }
-    }
-
-    //작은 값을 오른쪽으로 몰아낸다.
-    public static void DscendingSort(int[] list) {
-        for (int i=0; i<list.length; i++)
-        {
-            for(int j=0; j<list.length - (i +1); j++)
-            {
-                if(list[j] < list[j + 1])
+                if(isAscending)
                 {
-                    Swap(list, j, j+1);
+                    if(intArray[j] > intArray[j+1])
+                    {
+                        swap(intArray, j, j+1);
+                    }
+                }
+                else
+                {
+                    if(intArray[j] < intArray[j+1])
+                    {
+                        swap(intArray, j, j+1);
+                    }
                 }
             }
-
         }
     }
 }
